eefd30ea01b60701b06432da7071ff2df452884f
Emile Mistrot <mistrot.emile@gmail.com>
2023-11-03 09:35:34 +0100

    Create project
    Created springboot project

---
+++ src/main/java/com/incubateur/carpoolconnect/CarpoolconnectApplication.java
@@ -0,0 +1,13 @@
+package com.incubateur.carpoolconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarpoolconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarpoolconnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/incubateur/carpoolconnect/CarpoolconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.incubateur.carpoolconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarpoolconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
