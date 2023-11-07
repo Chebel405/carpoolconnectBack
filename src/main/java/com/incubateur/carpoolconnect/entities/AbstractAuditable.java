280d13e493debdf3d3787b96731315203c1eae99
Emile Mistrot <mistrot.emile@gmail.com>
2023-11-07 10:07:42 +0100

    Add audit
    Added audit entity and configuration

---
+++ src/main/java/com/incubateur/carpoolconnect/entities/AbstractAuditable.java
@@ -0,0 +1,33 @@
+package com.incubateur.carpoolconnect.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class AbstractAuditable {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime created;
+
+    @LastModifiedDate
+    private LocalDateTime updated;
+
+    @CreatedBy
+    @Column(name = "created_by", updatable = false)
+    private String createdBy;
+
+    @LastModifiedBy
+    @Column(name = "modified_by")
+    private String modifiedBy;
+
+}
